95bb6b4721b1f46880046b601eadff28168c27eb
madurangaPrabhath <maduracourse@gmail.com>
2025-10-28 21:50:31 +0530

    integrate Tailwind CSS and update routing structure & updated App component to include new routes for Home, Movies, MovieDetails, SeatLayout, Booking, and Favorite pages.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Router, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import Booking from './pages/Bookings'
+import Favorite from './pages/Favorite'
+import { Toaster } from 'react-hot-toast'
+import Footer from './components/Footer'
 
 const App = () => {
+  
+  const isAdminRoute = useLocation().pathname.startsWith('/admin');
+  
   return (
-    <div>
-      <Navbar />
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
       <Routes>
-        <Router />
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movie/:id/:date" element={<SeatLayout />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/favorite" element={<Favorite />} />
       </Routes>
-    </div>
+      {!isAdminRoute && <Footer />}
+    </>
   )
 }
 
