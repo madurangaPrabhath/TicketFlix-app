4c3156ca5c97c39fcd6739ea925dc2a6e6ef9ea9
madurangaPrabhath <maduracourse@gmail.com>
2025-10-31 18:08:17 +0530

    implement MovieDetails component with show details

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyDateTimeData, dummyShowsData } from '../assets/assets';
 
 const MovieDetails = () => {
-  return (
-    <div>MovieDetails</div>
-  )
+  const {id} = useParams();
+  const [show, setShow] = useState(null);
+
+  const getShow = async () => {
+    const show = dummyShowsData.find(show => show._id === id);
+    setShow({
+      movie: show,
+      dateTime: dummyDateTimeData
+    });
+  }
+
+  useEffect(() => {
+    getShow();
+  }, [id]);
+  
+  return show ? (
+    <div>
+      <div>
+        <img src={show.movie.poster_path} alt="" />
+        <div>
+          <BlurCircle />
+          <p>English</p>
+          <h1>{show.movie.title}</h1>
+          <div>
+            <StarIcon />
+            {show.movie.vote_average.toFixed(1)} User Rating
+          </div>
+          <p>{show.movie.overview}</p>
+
+          <p>{show.movie.runtime} . {show.movie.genres.map(genre => genre.name).join(", ")} . {show.movie.release_date.split("-")[0]} </p>
+
+          <div>
+            <button>
+              <PlayCircleIcon />
+              Watch Trailer
+            </button>
+            <a href="#dataSelect">Book Tickets</a>
+            <button>
+              <HeartIcon />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <p>Your Favourite Cast</p>
+      <div>
+        <div>
+          {show.movie.casts.slice(0,12).map((cast,index)=>(
+            <div key={index}>
+              <img src={cast.profile_path} alt={cast.name} />
+              <p>{cast.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  ) : <div>Loading...</div>
 }
 
 export default MovieDetails
