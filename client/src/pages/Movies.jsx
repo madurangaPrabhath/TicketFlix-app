02bd6d4c29b9d8003b5330ab208cf96695613090
madurangaPrabhath <maduracourse@gmail.com>
2025-10-31 17:15:06 +0530

    implemented Movies component to display available movies

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {dummyShowsData} from '../assets/assets'
 
 const Movies = () => {
-  return (
-    <div>Movies</div>
+  return dummyShowsData.length > 0 ? (
+    <div>
+      <h1>Now Showing</h1>
+      <div>
+        <BlurCircle />
+        <BlurCircle />
+        {dummyShowsData.map((movie) => (
+          <MovieCard movie={movie} key={movie._id} />
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div>
+      <h1>No Movies Available</h1>
+    </div>
   )
 }
 
