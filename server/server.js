a64568bb0bf685a25c048a687010842a5f2f859b
maduracourse <maduracourse@gmail.com>
2025-11-09 09:35:17 +0530

    implemented basic server setup in server.js

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => res.send('Server is running'));
+
+app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
