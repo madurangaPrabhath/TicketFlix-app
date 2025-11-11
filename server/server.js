65a17c0e5fe4a93330df53f86a8344938997e35d
maduracourse <maduracourse@gmail.com>
2025-11-11 21:45:50 +0530

    added booking router with CRUD operations and booking verification

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 import showRouter from "./routes/showRoutes.js";
 import movieRouter from "./routes/movieRoutes.js";
+import bookingRouter from "./routes/bookingRouters.js";
 
 const app = express();
 const port = 3000;
@@ -21,6 +22,7 @@ app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/shows", showRouter);
 app.use("/api/movies", movieRouter);
+app.use("/api/bookings", bookingRouter);
 
 app.listen(port, () =>
   console.log(`Server listening at http://localhost:${port}`)
